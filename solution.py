8c432f008df0c11d9c523f941b5184797757d081
SeregaLi <kaprall13rus@gmail.com>
2023-04-02 18:37:08 +0300

    1

--- solution.py
+++ solution.py
@@ -1,11 +1,19 @@
+import numpy
 import pandas as pd
 import numpy as np
+import scipy.stats as st
+
+chat_id = 876290128
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+def solution(x: np.ndarray) -> float:
+    v_mean = np.mean(x)
+    t = 10
+    n = len(x)
+    t_alpha2 = st.t.ppf(1 - 0.05 / 2, df=n - 1)
+    s = np.std(x, ddof=1)
+    delta = t_alpha2 * s / np.sqrt(n)
+    a = (v_mean - 0) / t * (1 - st.t.cdf(delta, df=n - 1))
+    return a + delta
 
-def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+
